cc6d3ae7370196543e6acbf9884e4860e641b42e
SubikshaRamkumar <114942854+SubikshaRamkumar@users.noreply.github.com>
2024-02-26 16:12:00 +0530

    Add files via upload

---
+++ GitHub Day Order wise/Day7/src/pages/user/LoanTypes.jsx
@@ -0,0 +1,12 @@
+import '../../assets/css/loantypes.css'
+const LoanTypes = () => {
+  return (
+    <div>
+      <div className="types_container">
+        
+      </div>
+    </div>
+  )
+}
+
+export default LoanTypes
