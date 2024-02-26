f9462ba1746673ec87ef851161adbe99b141dc6f
SubikshaRamkumar <114942854+SubikshaRamkumar@users.noreply.github.com>
2024-02-26 14:55:01 +0530

    Add files via upload

---
+++ GitHub Day Order wise/Day4/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+import '../assets/css/navbar.css'
+import logo from '../assets/images/logo.png'
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* <div className="logo1">
+        <div className="logo"></div>
+      </div> */} 
+      <a href="#"><img src={logo} className="logo"alt="logo"/></a>
+      <div className="menu">
+        <ul className='list'>
+            <li className='lists'><Link className="lin" to="/agrofunds/user/home">Home</Link></li>
+            <li className='lists'><Link className="lin"to="/agrofunds/user/about">About</Link></li>
+            <li className='lists'><Link className="lin" to="/agrofunds/user/contact">Contact</Link></li>
+            <li className='lists'><Link className="lin" to="/agrofunds/user/profile">Profile</Link></li>
+            {/* <li className='lists'><Link className="lin" to="/agrofunds/user/home">Types of loans</Link></li>
+            <li className='lists'><Link className="lin" to="/agrofunds/user/home">Apply Loan</Link></li>  */}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
